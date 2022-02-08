81b6ceb707f3cbdf72b2cb80340bbb79fd98a0be
HazemMarawan <30815783+HazemMarawan@users.noreply.github.com>
2022-02-08 02:07:19 +0200

    update

---
+++ ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRMS.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string old_password { get; set; }
+        public string new_password { get; set; }
+        public string new_password_confirm { get; set; }
+    }
+}
